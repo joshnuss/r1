046fbf74943f31df32a52ce229d73fc2d2ad4224
Josh Nussbaum <joshnuss@gmail.com>
2016-11-25 00:55:42 -0500

    Added subtraction

--- lib/r1/interpreter.ex
+++ lib/r1/interpreter.ex
@@ -1,4 +1,7 @@
 defmodule R1.Interpreter do
   def eval({:+, a, b}),
     do: a + b
+
+  def eval({:-, a, b}),
+    do: a - b
 end
---
+++ test/interpreter_test.exs
@@ -0,0 +1,13 @@
+defmodule R1.InterpreterTest do
+  use ExUnit.Case
+  import R1.Interpreter, only: [eval: 1]
+  doctest R1.Interpreter
+
+  test "adds values" do
+    assert eval({:+, 1, 2}) == 3
+  end
+
+  test "subtracts values" do
+    assert eval({:-, 7, 2}) == 5
+  end
+end
